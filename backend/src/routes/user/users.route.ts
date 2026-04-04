297dfff3eda7e3e23ea12faa09a57a6b1825e4c2
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-04-04 18:22:27 +0600

    fix(auth): remove auth middleware from login route to allow user sign-in

--- backend/src/routes/user/users.route.ts
+++ backend/src/routes/user/users.route.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import auth from "../../middlewares/auth.ts";
 import UsersController from "../../controllers/user/users.controller.ts";
 
 const usersRouter = Router();
 
 usersRouter.post("/register", UsersController.registration);
 
-usersRouter.post("/login", auth, UsersController.authentication);
+usersRouter.post("/login", UsersController.authentication);
 
 export default usersRouter;
